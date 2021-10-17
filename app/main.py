d8fd815120cca6a08bd1002ead1c091c094f2b0a
A. F. Kay <afkay.dev@pm.me>
2021-10-17 05:54:38 +0300

    Add allow CORS for frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from social.views import router
 
+origins = [
+    "http://localhost",
+    "http://localhost:9000",
+]
+
 app = FastAPI(dependencies=[], docs_url="/swagger", redoc_url=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router)
