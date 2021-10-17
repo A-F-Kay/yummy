a1be5f7d1a68e3f5980d12d7cf532a259647858f
A. F. Kay <afkay.dev@pm.me>
2021-10-17 07:31:03 +0300

    Add simple UI using Elastic UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import { HelloWorld } from 'components/HelloWorld';
+import { EuiHeader, EuiHeaderLink, EuiHeaderLinks, EuiHeaderLogo, EuiHeaderSectionItem } from '@elastic/eui';
+import { Page } from 'components/Page';
+import { Todo } from 'components/Todo';
 
 
-const App = () => <HelloWorld />;
+const App = () => (
+  <div>
+    <EuiHeader>
+      <EuiHeaderSectionItem border="right">
+        <EuiHeaderLogo iconType='monitoringApp'>Yummy</EuiHeaderLogo>
+      </EuiHeaderSectionItem>
+      <EuiHeaderSectionItem>
+        <EuiHeaderLinks aria-label="App navigation links example">
+          <EuiHeaderLink isActive>Docs</EuiHeaderLink>
+
+          <EuiHeaderLink>Code</EuiHeaderLink>
+
+          <EuiHeaderLink iconType="help">Help</EuiHeaderLink>
+        </EuiHeaderLinks>
+      </EuiHeaderSectionItem>
+    </EuiHeader>
+    <Page>
+      <Todo />
+    </Page>
+  </div>
+);
 
 export default App;
